96a41f25457a650f7166705b5809d65778416605
initsakthi <166901871+initsakthi@users.noreply.github.com>
2025-09-28 21:39:02 +0530

    Delete JavaDesignPatterns directory

--- JavaDesignPatterns/ObserverPattern/CurrentConditionsDisplay.java
+++
@@ -1,6 +0,0 @@
-public class CurrentConditionsDisplay implements Observer {
-    @Override
-    public void update(float temperature, float humidity) {
-        System.out.println("Current conditions: " + temperature + "°C and " + humidity + "% humidity");
-    }
-}
--- JavaDesignPatterns/SingletonPattern/SingletonDemo.java
+++
@@ -1,9 +0,0 @@
-public class SingletonDemo {
-    public static void main(String[] args) {
-        ConfigurationManager config1 = ConfigurationManager.getInstance();
-        config1.setConfig("Production");
-
-        ConfigurationManager config2 = ConfigurationManager.getInstance();
-        System.out.println("Config value: " + config2.getConfig());
-    }
-}
